35881796091782b59663adbb5915490d3322ef31
Rejean <rejean.lamy@maxt.com>
2022-08-25 13:54:40 -0400

    Added GreenSocketDemos folder

---
+++ GreenSocketDemos/Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GreenSocketDemos",
+    products: [
+        .executable(
+            name: "EchoServerDemo",
+            targets: ["EchoServerDemo"]),
+        .executable(
+            name: "EchoClientDemo",
+            targets: ["EchoClientDemo"]),
+        .executable(
+            name: "SimpleUDPDemo",
+            targets: ["SimpleUDPDemo"]),
+    ],
+    dependencies: [
+        .package(name: "GreenSocket", path: ".."),
+    ],
+    targets: [
+        .target(
+            name: "EchoServerDemo",
+            dependencies: [
+                .product(name: "Socket", package: "GreenSocket"),
+            ]),
+        .target(
+            name: "EchoClientDemo",
+            dependencies: [
+                .product(name: "Socket", package: "GreenSocket"),
+            ]),
+        .target(
+            name: "SimpleUDPDemo",
+            dependencies: [
+                .product(name: "Socket", package: "GreenSocket"),
+            ]),
+    ]
+)
